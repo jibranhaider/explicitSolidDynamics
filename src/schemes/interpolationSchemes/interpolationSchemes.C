bd6552ccdea48bb6fa55a5dbc1c0dee940519526
jibranhaider <jibranhaider@hotmail.com>
2019-02-15 13:00:35 +0000

    Refactor interpolation schemes code
    - Change format according to OpenFOAM style
    - Improve code design and readability

--- src/schemes/interpolationSchemes/interpolationSchemes.C
+++ src/schemes/interpolationSchemes/interpolationSchemes.C
@@ -25,54 +25,48 @@ License
 
 #include "interpolationSchemes.H"
 
-#include "addToRunTimeSelectionTable.H"
-#include "volFields.H"
-
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 namespace Foam
 {
 
 // * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
 defineTypeNameAndDebug(interpolationSchemes, 0);
 
 
-// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+// * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * * //
 
-  
-// * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * * //  
 interpolationSchemes::interpolationSchemes(const fvMesh& vm)
 :
-    mesh_(vm)
+    mesh_(vm),
+    own_(mesh_.owner()),
+    X_(mesh_.C()),
+    XF_(mesh_.Cf()),
+    XN_(mesh_.points())
 {}
-    
+
+
 // * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * * //
+
 interpolationSchemes::~interpolationSchemes()
 {}
 
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
-volVectorField interpolationSchemes::surfaceToCell     
+volVectorField interpolationSchemes::surfaceToVol
 (
-    const GeometricField<vector, fvsPatchField, surfaceMesh>& sf        
+    const GeometricField<vector, fvsPatchField, surfaceMesh>& sf
 ) const
-{   
-    const volVectorField& C = mesh_.C();
-    const surfaceVectorField& Cf = mesh_.Cf();
-    const scalar& maxInteriorFaceID = mesh_.nInternalFaces()-1;
-
-    const objectRegistry& db = mesh_.thisDb();
-    const pointVectorField& lmN_ = db.lookupObject<pointVectorField> ("lmN");   
-
-
+{
     tmp<GeometricField<vector, fvPatchField, volMesh> > tvf_v
     (
         new GeometricField<vector, fvPatchField, volMesh>
         (
             IOobject
             (
-                "surfaceTocell("+sf.name()+')',
+                "surfaceToVol("+sf.name()+')',
                 sf.instance(),
                 mesh_,
                 IOobject::NO_READ,
@@ -82,7 +76,6 @@ volVectorField interpolationSchemes::surfaceToCell
             dimensioned<vector>("0", sf.dimensions(), pTraits<vector>::zero)
         )
     );
-
     GeometricField<vector, fvPatchField, volMesh> U = tvf_v();
 
     tmp<GeometricField<scalar, fvPatchField, volMesh> > tvf_s
@@ -91,7 +84,7 @@ volVectorField interpolationSchemes::surfaceToCell
         (
             IOobject
             (
-                "surfaceTocell("+sf.name()+')',
+                "surfaceToVol("+sf.name()+')',
                 sf.instance(),
                 mesh_,
                 IOobject::NO_READ,
@@ -101,54 +94,61 @@ volVectorField interpolationSchemes::surfaceToCell
             dimensioned<scalar>("0", dimless, pTraits<scalar>::zero)
         )
     );
-
     GeometricField<scalar, fvPatchField, volMesh> w = tvf_s();
 
-    forAll (mesh_.faces(), faceID)
+    forAll(own_, faceID)
     {
-        if (faceID <= maxInteriorFaceID)
+        const label& ownID = own_[faceID];
+        const label& neiID = mesh_.neighbour()[faceID];
+
+        const vector& dOwn = XF_[faceID] - X_[ownID];
+        const vector& dNei = XF_[faceID] - X_[neiID];
+
+        U[ownID] += sf[faceID]*(1.0/mag(dOwn));
+        U[neiID] += sf[faceID]*(1.0/mag(dNei));
+
+        w[ownID] += 1.0/mag(dOwn);
+        w[neiID] += 1.0/mag(dNei);
+    }
+
+    forAll(mesh_.boundary(), patchID)
+    {
+        forAll(mesh_.boundary()[patchID], facei)
         {
-            const label& ownID = mesh_.owner()[faceID];
-            const label& neiID = mesh_.neighbour()[faceID];
+            const label& bCellID =
+                mesh_.boundaryMesh()[patchID].faceCells()[facei];
 
-            const vector& dOwn = Cf[faceID] - C[ownID]; 
-            const vector& dNei = Cf[faceID] - C[neiID];             
-
-            U[ownID] += sf[faceID] * (1.0/mag(dOwn));
-            U[neiID] += sf[faceID] * (1.0/mag(dOwn));
-
-            w[ownID] += 1.0/mag(dOwn);
-            w[neiID] += 1.0/mag(dNei);
-        }
-        else
-        {
-            const label& patchID = mesh_.boundaryMesh().whichPatch(faceID);         
-            const label& facei = mesh_.boundaryMesh()[patchID].whichFace(faceID);               
-            const label& bCellID = mesh_.boundaryMesh()[patchID].faceCells()[facei];
-            vector d = Cf.boundaryField()[patchID][facei] - C[bCellID];
+            vector d = XF_.boundaryField()[patchID][facei] - X_[bCellID];
 
             U[bCellID] += sf.boundaryField()[patchID][facei] * (1.0/mag(d));
             w[bCellID] += 1.0/mag(d);
- 
-            if (lmN_.boundaryField().types()[patchID] == "fixedValue")
-            {   
-                const pointVectorField& xN_0_ = db.lookupObject<pointVectorField> ("xN_0");     
-                const label& faceID = mesh_.boundary()[patchID].start() + facei;
 
-                forAll (mesh_.faces()[faceID], nodei)
+            const objectRegistry& db = mesh_.thisDb();
+
+            const pointVectorField& lmN_ =
+                db.lookupObject<pointVectorField> ("lmN");
+
+            if (lmN_.boundaryField().types()[patchID] == "fixedValue")
+            {
+                const label& faceID =
+                    mesh_.boundary()[patchID].start() + facei;
+
+                forAll(mesh_.faces()[faceID], nodei)
                 {
-                    const label& nodeID = mesh_.faces()[faceID][nodei];             
-            
-                    d = xN_0_[nodeID] - C[bCellID];                         
+                    const label& nodeID = mesh_.faces()[faceID][nodei];
+                    d = XN_[nodeID] - X_[bCellID];
                     U[bCellID] += lmN_[nodeID] * (1.0/mag(d));
-                    w[bCellID] += (1.0/mag(d));                     
-                    
+                    w[bCellID] += (1.0/mag(d));
+
                     for (int i=0; i<7; i++)
-                    {                                       
-                        d = ( ( ((i+1)*xN_0_[nodeID]) + ((7-i)*Cf.boundaryField()[patchID][facei]) ) / 8.0 ) - C[bCellID];                          
-                
-                        U[bCellID] +=   lmN_[nodeID] * (1.0/mag(d));
-                        w[bCellID] += (1.0/mag(d));                 
+                    {
+                        d =
+                            ((((i+1)*XN_[nodeID])
+                          + ((7-i)*XF_.boundaryField()[patchID][facei]) )/8.0)
+                          - X_[bCellID];
+
+                        U[bCellID] += lmN_[nodeID] * (1.0/mag(d));
+                        w[bCellID] += 1.0/mag(d);
                     }
                 }
             }
@@ -167,15 +167,12 @@ volVectorField interpolationSchemes::surfaceToCell
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 template<class Type>
-void interpolationSchemes::pushUntransformedDataNew
+void interpolationSchemes::pushUntransformedData
 (
     List<Type>& pointData
 ) const
 {
-    const fvMesh& mesh = mesh_; 
-    
-    // Transfer onto coupled patch
-    const globalMeshData& gmd = mesh.globalData();
+    const globalMeshData& gmd = mesh_.globalData();
     const indirectPrimitivePatch& cpp = gmd.coupledPatch();
     const labelList& meshPoints = cpp.meshPoints();
 
@@ -188,22 +185,17 @@ void interpolationSchemes::pushUntransformedDataNew
         elems[i] = pointData[meshPoints[i]];
     }
 
-    // Combine master data with slave data
     forAll(slaves, i)
     {
         const labelList& slavePoints = slaves[i];
-
-        // Copy master data to slave slots
         forAll(slavePoints, j)
         {
             elems[slavePoints[j]] = elems[i];
         }
     }
 
-    // Push slave-slot data back to slaves
     slavesMap.reverseDistribute(elems.size(), elems, false);
 
-    // Extract back onto mesh
     forAll(meshPoints, i)
     {
         pointData[meshPoints[i]] = elems[i];
@@ -214,69 +206,63 @@ void interpolationSchemes::pushUntransformedDataNew
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 template<class Type>
-void interpolationSchemes::addSeparatedNew
+void interpolationSchemes::addSeparated
 (
     GeometricField<Type, pointPatchField, pointMesh>& pf
 ) const
 {
+    typename GeometricField<Type, pointPatchField, pointMesh>::
+        Internal& pfi = pf.ref();
 
-     if (debug)
-     {
-         Pout<< "volPointInterpolation::addSeparated" << endl;
-     }
- 
-     typename GeometricField<Type, pointPatchField, pointMesh>::
-         Internal& pfi = pf.ref();
- 
-     typename GeometricField<Type, pointPatchField, pointMesh>::
-         Boundary& pfbf = pf.boundaryFieldRef();
- 
-     forAll(pfbf, patchi)
-     {
-         if (pfbf[patchi].coupled())
-         {
-             refCast<coupledPointPatchField<Type>>
-                 (pfbf[patchi]).initSwapAddSeparated
-                 (
-                     Pstream::commsTypes::nonBlocking,
-                     pfi
-                 );
-         }
-     }
- 
-     // Block for any outstanding requests
-     Pstream::waitRequests();
- 
-     forAll(pfbf, patchi)
-     {
-         if (pfbf[patchi].coupled())
-         {
-             refCast<coupledPointPatchField<Type>>
-                 (pfbf[patchi]).swapAddSeparated
-                 (
-                     Pstream::commsTypes::nonBlocking,
-                     pfi
-                 );
-         }
+    typename GeometricField<Type, pointPatchField, pointMesh>::
+        Boundary& pfbf = pf.boundaryFieldRef();
+
+    forAll(pfbf, patchi)
+    {
+        if (pfbf[patchi].coupled())
+        {
+            refCast<coupledPointPatchField<Type>>
+                (pfbf[patchi]).initSwapAddSeparated
+                (
+                    Pstream::commsTypes::nonBlocking,
+                    pfi
+                );
+        }
+    }
+
+    Pstream::waitRequests();
+
+    forAll(pfbf, patchi)
+    {
+        if (pfbf[patchi].coupled())
+        {
+            refCast<coupledPointPatchField<Type>>
+                (pfbf[patchi]).swapAddSeparated
+                (
+                    Pstream::commsTypes::nonBlocking,
+                    pfi
+                );
+        }
     }
 }
 
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-void interpolationSchemes::volToPoint   
+void interpolationSchemes::volToPoint
 (
     const GeometricField<vector, fvPatchField, volMesh>& U,
     const GeometricField<tensor, fvPatchField, volMesh>& Ugrad,
-    GeometricField<vector, pointPatchField, pointMesh>& Un                  
+    GeometricField<vector, pointPatchField, pointMesh>& Un
 ) const
 {
- 
-    const fvMesh& mesh = mesh_; 
-    const volVectorField& C = mesh.C();    
 
-    if( Pstream::parRun() ) 
-    {           
+    const fvMesh& mesh = mesh_;
+    const volVectorField& C = mesh.C();
+
+
+    if( Pstream::parRun() )
+    {
         pointScalarField sum
         (
             IOobject
@@ -287,112 +273,136 @@ void interpolationSchemes::volToPoint
             ),
             pointMesh::New(mesh),
             dimensionedScalar("zero", dimless, 0.0)
-        );      
+        );
 
         forAll (mesh.points(), nodeID)
         {
             Un[nodeID] = vector::zero;
-        }   
+        }
 
         forAll (mesh.points(), nodeID)
         {
             forAll (mesh.pointCells()[nodeID], cell)
-            {   
+            {
                 const label& cellID = mesh.pointCells()[nodeID][cell];
                 const vector& d = mesh.points()[nodeID] - C[cellID];
-                const vector& recons = U[cellID] + ( Ugrad[cellID] & d );   
+                const vector& recons = U[cellID] + ( Ugrad[cellID] & d );
                 const scalar& weight = 1;
-                
+
                 Un[nodeID] += recons;
-                sum[nodeID] += weight;      
+                sum[nodeID] += weight;
             }
-        }       
+        }
 
         pointConstraints::syncUntransformedData(mesh, sum, plusEqOp<scalar>());
-        addSeparatedNew(sum);
-        pushUntransformedDataNew(sum);
-    
+        addSeparated(sum);
+        pushUntransformedData(sum);
+
         forAll (mesh.points(), nodeID)
-        {       
+        {
             Un[nodeID] = Un[nodeID] / sum[nodeID];
         }
-    
+
         pointConstraints::syncUntransformedData(mesh, Un, plusEqOp<vector>());
-        addSeparatedNew(Un);
-        pushUntransformedDataNew(Un);
+        addSeparated(Un);
+        pushUntransformedData(Un);
     }
-    
+
     else
     {
         forAll (mesh.pointCells(), nodeID)
         {
             vector sum = vector::zero;
             scalar weights = 0.0;
-            
+
             forAll (mesh.pointCells()[nodeID], cell)
             {
                 const label& cellID = mesh.pointCells()[nodeID][cell];
                 const vector& d = mesh.points()[nodeID] - C[cellID];
                 const vector& recons = U[cellID] + ( Ugrad[cellID] & d );
-                    
+
                 //sum += recons * (1.0/mag(d));
                 //weights += (1.0/mag(d));
-                
+
                 sum += recons;
                 weights += 1.0;
             }
-            
-            Un[nodeID] = sum / weights; 
-        }       
-    }   
+
+            Un[nodeID] = sum / weights;
+        }
+    }
+
 }
 
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-void interpolationSchemes::pointToSurface
+surfaceVectorField interpolationSchemes::pointToSurface
 (
-    const GeometricField<vector, pointPatchField, pointMesh>& U,
-    GeometricField<vector, fvsPatchField, surfaceMesh>& Uf                  
+    const GeometricField<vector, pointPatchField, pointMesh>& U
 ) const
 {
-    const fvMesh& mesh = mesh_;
-    const scalar& maxInteriorFaceID = mesh.nInternalFaces()-1;  
+    vector d = vector::zero;
+    vector sum = vector::zero;
+    scalar weights = 0.0;
 
-    forAll (mesh.faces(),faceID)
+    tmp<GeometricField<vector, fvsPatchField, surfaceMesh> > tsf_v
+    (
+        new GeometricField<vector, fvsPatchField, surfaceMesh>
+        (
+            IOobject
+            (
+                "pointToSurface("+U.name()+')',
+                U.instance(),
+                mesh_,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            mesh_,
+            dimensioned<vector>("0", U.dimensions(), pTraits<vector>::zero)
+        )
+    );
+    GeometricField<vector, fvsPatchField, surfaceMesh> Uf = tsf_v();
+
+    forAll(own_, faceID)
     {
-        vector sum = vector::zero;
-        scalar weights = 0.0;
+        sum = vector::zero;
+        weights = 0.0;
 
-        if (faceID <= maxInteriorFaceID)
-        {   
-            forAll (mesh.faces()[faceID],node)
-            {
-                const label& nodeID = mesh.faces()[faceID][node];
-                sum += U[nodeID];
-                weights += 1.0;                             
-            }
-            
-            Uf[faceID] = sum / weights; 
-        }
-        
-        else
+        forAll(mesh_.faces()[faceID], node)
         {
-            const label& patchID = mesh.boundaryMesh().whichPatch(faceID);
-            const label& facei = mesh.boundaryMesh()[patchID].whichFace(faceID);
+            const label& nodeID = mesh_.faces()[faceID][node];
+            d = XN_[nodeID] - XF_[faceID];
+            sum += U[nodeID]*(1.0/mag(d));
+            weights += 1.0/mag(d);
+        }
 
-            forAll (mesh.faces()[faceID],node)
-            {
-                const label& nodeID = mesh.faces()[faceID][node];       
-                sum += U[nodeID];
-                weights += 1.0;                             
-            }
-            
-            Uf.boundaryFieldRef()[patchID][facei]= sum / weights;           
-        }   
+        Uf[faceID] = sum/weights;
     }
-}
 
+    forAll(mesh_.boundary(), patchID)
+    {
+        forAll(mesh_.boundary()[patchID], facei)
+        {
+            const label& faceID = mesh_.boundary()[patchID].start() + facei;
+            sum = vector::zero;
+            weights = 0.0;
+
+            forAll(mesh_.faces()[faceID], node)
+            {
+                const label& nodeID = mesh_.faces()[faceID][node];
+                d = XN_[nodeID] - XF_.boundaryField()[patchID][facei];
+                sum += U[nodeID]*(1.0/mag(d));
+                weights += 1.0/mag(d);
+            }
+
+            Uf.boundaryFieldRef()[patchID][facei] = sum/weights;
+        }
+    }
+
+    tsf_v.clear();
+    return Uf;
+}
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
